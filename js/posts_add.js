cd59a471747c5b0dbb2c416cc9439ddcd378c36f
tara manicsic <tzmanics@gmail.com>
2013-12-22 19:11:20 -0500

    form work: +required fields, +img prvw; update readme

--- js/posts_add.js
+++ js/posts_add.js
@@ -10,5 +10,26 @@ var options = {
     } 
 }; 
 
+$('#image1').blur(function(){
+
+	var image1Input = $(this).val();
+	$('#resultImage1').empty().append('<img src="'+ image1Input+'">');
+});
+$('#image2').blur(function(){
+
+	var image2Input = $(this).val();
+	$('#resultImage2').empty().append('<img src="'+ image2Input+'">');
+});
+$('#image3').blur(function(){
+
+	var image3Input = $(this).val();
+	$('#resultImage3').empty().append('<img src="'+ image3Input+'">');
+});
+$('#image4').blur(function(){
+
+	var image4Input = $(this).val();
+	$('#resultImage4').empty().append('<img src="'+ image4Input+'">');
+});
+
 // Using the above options, ajax'ify the form
 $('form').ajaxForm(options);
--- views/v_posts_add.php
+++ views/v_posts_add.php
@@ -1,23 +1,29 @@
 <div class="forms">
 	<form method ='post' action='/posts/p_add'>
 	<h1>CREATE POST</h1><br>
-	Post Title <input type='text' name='post_title'><br><br>
-	Description<textarea name = 'post_description' id = 'post_description'></textarea>
+	Post Title : <input required type='text' name='post_title' required type='text'><br><br>
+	Description : <textarea required name='post_description' id = 'post_description' required type='text'></textarea>
 	<br><br>
 
-	<label for="image1">Image 1:</label>
-	Image Link<input type="text" name="image1" id="image1"><br>
-	Image Caption<input type='text' name='image1_cap'><br>
+	Image 1 Link : <input type="text" name="image1" id="image1"><br>
+	Image 1 Caption : <input type='text' name='image1_cap'><br>
 
-	Image Link<input type="text" name="image2" id="image2"><br>
-	Image Caption<input type='text' name='image2_cap'><br>
+	Image 2 Link : <input type="text" name="image2" id="image2"><br>
+	Image 2 Caption : <input type='text' name='image2_cap'><br>
 
-	Image Link<input type="text" name="image3" id="image3"><br>
-	Image Caption<input type='text' name='image3_cap'><br>
+	Image 3 Link : <input type="text" name="image3" id="image3"><br>
+	Image 3 Caption : <input type='text' name='image3_cap'><br>
 
-	Image Link<input type="text" name="image4" id="image4"><br>
-	Image Caption<input type='text' name='image4_cap'><br>
+	Image 4 Link : <input type="text" name="image4" id="image4"><br>
+	Image 4 Caption : <input type='text' name='image4_cap'><br>
 	<input type = 'Submit' value = 'POST'>
 	</form>
+
+<div id='results'>
+look for your images here after you add the link to make sure it's correct before you submit :)</div>
+<div id='resultImage1'>image 1</div>
+<div id='resultImage2'>image 2</div>
+<div id='resultImage3'>image 3</div>
+<div id='resultImage4'>image 4</div>
+
 </div>
-<div id='results'></div>
