ee12f044052a4553eb0c64995e709665ae60c27b
tara manicsic <tzmanics@gmail.com>
2013-12-22 20:12:40 -0500

    alt prob

--- views/v_posts_pages.php
+++ views/v_posts_pages.php
@@ -13,7 +13,7 @@
 		<!-- for loop to display all images -->
 		<?php 
 		for ($x=1; $x<=4; $x++){
-			echo "<img src='".$post['image'.$x]."' alt='post image'>";
+			echo "<img src='".$post['image'.$x]."'>";
 			echo "<p>".$post['image'.$x.'_cap']."</p>";
 		}?>
 	<?php endforeach; ?>
